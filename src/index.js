e4214b12f2425551cd034a8e2289f9a7146fac5e
jacobrees <jacob.rees97@yahoo.com>
2021-08-25 09:56:31 +0100

    Set up basic Phaser configuration inside index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './styles.scss';
+
+const Phaser = require('phaser');
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
+    },
+  },
+  scene: {
+    preload,
+    create,
+    update,
+  },
+};
+
+const game = new Phaser.Game(config);
