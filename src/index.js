8c3d46b37a51b70fec10dfac40dfcff7b2ebb0d1
jacobrees <jacob.rees97@yahoo.com>
2021-08-27 16:38:03 +0100

    Add rocket to start screen

--- src/index.js
+++ src/index.js
@@ -10,13 +10,26 @@ class StartScreen extends Phaser.Scene {
 
   preload() {
     this.load.image('background', './assets/space-background.png');
+    this.load.spritesheet('rocket-flicker', './assets/rocket-flicker.png', { frameWidth: 256, frameHeight: 582 });
+    this.load.image('rocket', './assets/rocket.png');
   }
 
   create() {
     background = this.add.tileSprite(230, 320, 460, 640, 'background');
 
+    this.add.image(230, 320, 'rocket').setScale(0.5).setAngle(135);
+    const rocketFlicker = this.add.sprite(130, 220, 'rocket-flicker', 0).setScale(0.25).setAngle(135);
     this.add.text(230, 50, 'Space Dodger', { fontSize: '42px' }).setOrigin(0.5);
     this.add.text(230, 580, 'Start', { fontSize: '42px' }).setOrigin(0.5);
+
+    this.anims.create({
+      key: 'rocketFlicker',
+      frames: this.anims.generateFrameNumbers('rocket-flicker'),
+      frameRate: 8,
+      repeat: -1,
+    });
+
+    rocketFlicker.play('rocketFlicker');
   }
 
   update() { //eslint-disable-line
