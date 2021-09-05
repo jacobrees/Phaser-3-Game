12122f7006f77d096b8e3c24a4470e3f8518a565
jacobrees <jacob.rees97@yahoo.com>
2021-09-05 22:54:33 +0100

    Remove game play text from game play scene

--- src/javascript/gamePlay.js
+++ src/javascript/gamePlay.js
@@ -13,7 +13,6 @@ class GamePlay extends Phaser.Scene {
 
   create() {
     background = this.add.tileSprite(230, 320, 460, 640, 'gamePlayBackground');
-    this.add.bitmapText(230, 50, 'press-start-2p', 'GamePlay', 35).setOrigin(0.5);
   }
 
   update() { //eslint-disable-line
