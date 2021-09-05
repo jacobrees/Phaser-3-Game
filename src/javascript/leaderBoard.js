13ec50c9dd9e3273b4d0955b052987bdf7523d5d
jacobrees <jacob.rees97@yahoo.com>
2021-09-05 22:41:47 +0100

    Add main menu button to leader board scene

--- src/javascript/leaderBoard.js
+++ src/javascript/leaderBoard.js
@@ -13,7 +13,14 @@ class LeaderBoard extends Phaser.Scene {
 
   create() {
     background = this.add.tileSprite(230, 320, 460, 640, 'background');
+
     this.add.bitmapText(230, 50, 'press-start-2p', 'LeaderBoard', 35).setOrigin(0.5);
+
+    this.add.rectangle(230, 560, 267, 42, 0x6666ff).setInteractive({ cursor: 'pointer' })
+      .on('pointerdown', () => {
+        this.scene.start('MainMenu');
+      });
+    this.add.bitmapText(230, 560, 'press-start-2p', 'MainMenu', 28).setOrigin(0.5);
   }
 
   update() { //eslint-disable-line
