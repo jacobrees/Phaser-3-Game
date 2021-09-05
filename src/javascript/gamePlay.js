37aa051011792821f5eb4478251ca0d1f95d1054
jacobrees <jacob.rees97@yahoo.com>
2021-09-05 21:02:05 +0100

    Add moving background to gameplay scene

--- src/javascript/gamePlay.js
+++ src/javascript/gamePlay.js
@@ -1,19 +1,23 @@
 import Phaser from 'phaser';
 
+let background;
+
 class GamePlay extends Phaser.Scene {
   constructor() {
     super({ key: 'GamePlay' });
   }
 
   preload() {
+    this.load.image('background', './assets/img/space-background.png');
   }
 
   create() {
+    background = this.add.tileSprite(230, 320, 460, 640, 'background');
     this.add.bitmapText(230, 50, 'press-start-2p', 'GamePlay', 35).setOrigin(0.5);
   }
 
   update() { //eslint-disable-line
-
+    background.tilePositionY += 7;
   }
 }
 
