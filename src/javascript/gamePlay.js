15e1891386515db0144475692422bacf75497fff
jacobrees <jacob.rees97@yahoo.com>
2021-09-20 14:46:34 +0100

    Add spinning star that rocket can collect and increase score by 10 points to gameplay

--- src/javascript/gamePlay.js
+++ src/javascript/gamePlay.js
@@ -1,7 +1,8 @@
 import Phaser from 'phaser';
 
 let background;
-const score = 0;
+let star;
+let score = 0;
 
 class GamePlay extends Phaser.Scene {
   constructor() {
@@ -12,6 +13,7 @@ class GamePlay extends Phaser.Scene {
     this.load.image('gamePlayBackground', './assets/img/game-play-background.png');
     this.load.spritesheet('rocket-flicker', './assets/spritesheet/rocket-flicker.png', { frameWidth: 256, frameHeight: 581 });
     this.load.image('rocket', './assets/img/rocket.png');
+    this.load.image('star', './assets/img/star.png');
     this.load.bitmapFont('press-start-2p', './assets/bitmap/PressStart2P.png', './assets/bitmap/PressStart2P.xml');
   }
 
@@ -26,7 +28,7 @@ class GamePlay extends Phaser.Scene {
     };
 
     const rocket = this.physics.add.image(setSpawnX(), 130, 'rocket').setScale(0.25).setAngle(180);
-    rocket.body.setSize(rocket.width - 340, rocket.height - 190, true).setOffset(172, 135);
+    rocket.body.setSize(rocket.width - 350, rocket.height - 200, true).setOffset(178, 135);
 
     const rocketFlicker = this.add.sprite(setSpawnX(), 60, 'rocket-flicker', 0).setScale(0.13).setAngle(180);
 
@@ -44,11 +46,29 @@ class GamePlay extends Phaser.Scene {
       rocketFlicker.x = Phaser.Math.Clamp(pointer.x, 50, 410);
     });
 
-    this.add.bitmapText(10, 10, 'press-start-2p', `Score:${score}`, 20).setOrigin(0);
+    star = this.physics.add.image(230, 430, 'star').setScale(0.075);
+    star.body.setSize(star.width - 145, star.height - 145, true).setOffset(75, 75);
+    star.setVelocityY(-200);
+
+    const scoreText = this.add.bitmapText(10, 10, 'press-start-2p', `Score:${score}`, 20).setOrigin(0);
+
+    const handleCollectStar = (star) => {
+      star.destroy();
+      score += 10;
+      scoreText.text = `Score:${score}`;
+    };
+
+    this.physics.add.overlap(
+      star,
+      rocket,
+      handleCollectStar,
+      undefined,
+    );
   }
 
   update() { //eslint-disable-line
     background.tilePositionY += 20;
+    star.angle += 3;
   }
 }
 
