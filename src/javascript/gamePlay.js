a62611ba30efdf32fc6b10d28fc193c7076206bc
jacobrees <jacob.rees97@yahoo.com>
2021-09-20 16:04:26 +0100

    Set stars to reset x & y position & velocity at the bottom of screen after collected by rocket

--- src/javascript/gamePlay.js
+++ src/javascript/gamePlay.js
@@ -20,17 +20,17 @@ class GamePlay extends Phaser.Scene {
   create() {
     background = this.add.tileSprite(230, 320, 460, 640, 'gamePlayBackground');
 
-    const setSpawnX = () => {
+    const setRocketSpawnX = () => {
       if (this.input.mousePointer.x !== 0) {
         return this.input.mousePointer.x;
       }
       return 230;
     };
 
-    const rocket = this.physics.add.image(setSpawnX(), 130, 'rocket').setScale(0.25).setAngle(180);
+    const rocket = this.physics.add.image(setRocketSpawnX(), 130, 'rocket').setScale(0.25).setAngle(180);
     rocket.body.setSize(rocket.width - 350, rocket.height - 200, true).setOffset(178, 135);
 
-    const rocketFlicker = this.add.sprite(setSpawnX(), 60, 'rocket-flicker', 0).setScale(0.13).setAngle(180);
+    const rocketFlicker = this.add.sprite(setRocketSpawnX(), 60, 'rocket-flicker', 0).setScale(0.13).setAngle(180);
 
     this.anims.create({
       key: 'rocketFlicker',
@@ -46,14 +46,18 @@ class GamePlay extends Phaser.Scene {
       rocketFlicker.x = Phaser.Math.Clamp(pointer.x, 50, 410);
     });
 
-    star = this.physics.add.image(230, 730, 'star').setScale(0.075);
+    const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+    star = this.physics.add.image(randomNumber(50, 410), randomNumber(710, 2500), 'star').setScale(0.075);
     star.body.setSize(star.width - 145, star.height - 145, true).setOffset(75, 75);
-    star.setVelocityY(-200);
+    star.setVelocityY(randomNumber(-200, -700));
 
     const scoreText = this.add.bitmapText(10, 10, 'press-start-2p', `Score:${score}`, 20).setOrigin(0);
 
     const handleCollectStar = () => {
-      star.destroy();
+      star.x = randomNumber(50, 410);
+      star.y = randomNumber(710, 2500);
+      star.setVelocityY(randomNumber(-200, -700));
       score += 10;
       scoreText.text = `Score:${score}`;
     };
@@ -68,7 +72,7 @@ class GamePlay extends Phaser.Scene {
 
   update() { //eslint-disable-line
     background.tilePositionY += 20;
-    star.angle += 1;
+    star.angle += 2;
   }
 }
 
