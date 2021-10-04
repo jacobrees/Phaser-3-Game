a4c11fbf6a569ef57be20ef903648e1b692debb3
jacobrees <jacob.rees97@yahoo.com>
2021-10-04 23:44:27 +0100

    Add update username button inside of settings

--- src/javascript/gameSettings.js
+++ src/javascript/gameSettings.js
@@ -2,8 +2,6 @@ import Phaser from 'phaser';
 import globalState from './globalState.js';
 
 let background;
-let musicRectangle;
-let effectsRectangle;
 
 class GameSettings extends Phaser.Scene {
   constructor() {
@@ -20,17 +18,24 @@ class GameSettings extends Phaser.Scene {
 
     this.add.bitmapText(230, 50, 'press-start-2p', 'Settings', 35).setOrigin(0.5);
 
-    musicRectangle = this.add.rectangle(230, 150, 275, 52, 0x6666ff).setInteractive({ cursor: 'pointer' })
+    this.add.rectangle(230, 135, 245, 70, 0x6666ff).setInteractive({ cursor: 'pointer' })
+      .on('pointerup', () => {
+        this.scene.start('SetUsername');
+      });
+    this.add.bitmapText(230, 120, 'press-start-2p', 'Update', 28).setOrigin(0.5);
+    this.add.bitmapText(230, 150, 'press-start-2p', 'Username', 28).setOrigin(0.5);
+
+    musicRectangle = this.add.rectangle(230, 230, 275, 52, 0x6666ff).setInteractive({ cursor: 'pointer' })
       .on('pointerup', () => {
         globalState.music = !globalState.music;
       });
-    this.add.bitmapText(230, 150, 'press-start-2p', 'GameMusic', 28).setOrigin(0.5);
+    this.add.bitmapText(230, 230, 'press-start-2p', 'GameMusic', 28).setOrigin(0.5);
 
-    effectsRectangle = this.add.rectangle(230, 230, 225, 52, 0x6666ff).setInteractive({ cursor: 'pointer' })
+    effectsRectangle = this.add.rectangle(230, 310, 225, 52, 0x6666ff).setInteractive({ cursor: 'pointer' })
       .on('pointerup', () => {
         globalState.effects = !globalState.effects;
       });
-    this.add.bitmapText(230, 230, 'press-start-2p', 'GameSFX', 28).setOrigin(0.5);
+    this.add.bitmapText(230, 310, 'press-start-2p', 'GameSFX', 28).setOrigin(0.5);
 
     this.add.rectangle(230, 560, 255, 52, 0x6666ff).setInteractive({ cursor: 'pointer' })
       .on('pointerdown', () => {
@@ -42,18 +47,6 @@ class GameSettings extends Phaser.Scene {
   update() { //eslint-disable-line
     background.tilePositionY += 2;
     background.tilePositionX += 2;
-
-    if (globalState.music) {
-      musicRectangle.setFillStyle(0x6666ff, 1.0);
-    } else {
-      musicRectangle.setFillStyle(0x4E4F51, 1.0);
-    }
-
-    if (globalState.effects) {
-      effectsRectangle.setFillStyle(0x6666ff, 1.0);
-    } else {
-      effectsRectangle.setFillStyle(0x4E4F51, 1.0);
-    }
   }
 }
 
