10a7435339154bddd280b39cbc22bb61cc90a692
jacobrees <jacob.rees97@yahoo.com>
2021-10-05 01:18:02 +0100

    Add exception handling to username form

--- src/javascript/setUsername.js
+++ src/javascript/setUsername.js
@@ -17,10 +17,10 @@ class SetUsername extends Phaser.Scene {
 
     this.add.bitmapText(230, 50, 'press-start-2p', 'Username', 38).setOrigin(0.5);
 
-    this.add.bitmapText(230, 100, 'press-start-2p', 'Username Can Only Include', 16).setOrigin(0.5);
-    this.add.bitmapText(230, 130, 'press-start-2p', 'Characters a-z A-Z 0-9', 16).setOrigin(0.5);
-    this.add.bitmapText(230, 170, 'press-start-2p', 'Username Can Only Be', 16).setOrigin(0.5);
-    this.add.bitmapText(230, 200, 'press-start-2p', '3-8 Characters Long', 16).setOrigin(0.5);
+    const warningText1 = this.add.bitmapText(230, 100, 'press-start-2p', 'Username Can Only Include', 16).setOrigin(0.5);
+    const warningText2 = this.add.bitmapText(230, 130, 'press-start-2p', 'Characters a-z A-Z 0-9', 16).setOrigin(0.5);
+    const warningText3 = this.add.bitmapText(230, 170, 'press-start-2p', 'Username Can Only Be', 16).setOrigin(0.5);
+    const warningText4 = this.add.bitmapText(230, 200, 'press-start-2p', '3-8 Characters Long', 16).setOrigin(0.5);
 
     this.add.bitmapText(230, 260, 'press-start-2p', 'Type Username Here', 12).setOrigin(0.5);
 
@@ -42,9 +42,38 @@ class SetUsername extends Phaser.Scene {
     this.add.rectangle(370, 302, 10, 58, 0x6666ff).setOrigin(0, 0.5);
     this.add.rectangle(460, 302, 80, 52, 0x121212).setOrigin(1, 0.5);
 
+    const charactersWarningText = [warningText1, warningText2];
+    const lengthWarningText = [warningText3, warningText4];
+
+    const regex = new RegExp('^[a-zA-Z0-9]*$');
+
     this.add.rectangle(230, 410, 277, 52, 0x6666ff).setInteractive({ cursor: 'pointer' })
       .on('pointerdown', () => {
-        this.scene.start('MainMenu');
+        if (text.text.length < 3 && !regex.test(text.text)) {
+          lengthWarningText.forEach((warningText) => {
+            warningText.tint = 0xff0008;
+          });
+          charactersWarningText.forEach((warningText) => {
+            warningText.tint = 0xff0008;
+          });
+        } else if (text.text.length > 8 && !regex.test(text.text)) {
+          lengthWarningText.forEach((warningText) => {
+            warningText.tint = 0xff0008;
+          });
+          charactersWarningText.forEach((warningText) => {
+            warningText.tint = 0xff0008;
+          });
+        } else if (text.text.length < 3 || text.text.length > 8) {
+          lengthWarningText.forEach((warningText) => {
+            warningText.tint = 0xff0008;
+          });
+        } else if (!regex.test(text.text)) {
+          charactersWarningText.forEach((warningText) => {
+            warningText.tint = 0xff0008;
+          });
+        } else {
+          this.scene.start('MainMenu');
+        }
       });
     this.add.bitmapText(230, 410, 'press-start-2p', 'Submit', 28).setOrigin(0.5);
   }
